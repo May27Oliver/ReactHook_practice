585b664d7054208d9236637439b59723d32efe8e
May27Oliver <oliver310338@hotmail.com>
2021-02-06 21:41:32 +0800

    try gist

--- src/api/todos.js
+++ src/api/todos.js
@@ -1,8 +1,14 @@
-const baseURL = 'http://localhost:3001';
+const baseURL = 'https://gist.githubusercontent.com/May27Oliver/65cbf1224fc415817434715772da2443/raw/b1fc0afb1934594615f5d22fa2720ec5528f322d/todo.json';
 
 //READ
 export const getTodos = () =>{
-    return fetch(`${baseURL}/info`).then(res =>res.json());
+    return fetch(`${baseURL}/info`,{
+        method:'GET',
+        headers:{
+            'Content-Type':'application/json'
+        }
+    }
+    ).then(res =>res.json());
 }
 //CREATE
 export const createTodo = async (payload) =>{
